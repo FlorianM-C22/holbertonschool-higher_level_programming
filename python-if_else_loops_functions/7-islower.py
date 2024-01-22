0c115f214c8b4be4c725b8d341c7aeb4e2fbb8aa
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-22 19:05:33 +0100

    Task 7 - Local test = OK Added .gitignore to avoid pushing pycache

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,18 @@
+def islower(c):
+    """
+    Checks for lowercase character.
+
+    Parameters:
+    - c (str): The character to check
+
+    Returns:
+    bool: True if the character is a lowercase letter, False otherwise.
+
+    """
+
+    to_ascii = ord(c)
+
+    if to_ascii in range(97, 123):
+        return True
+    else:
+        return False
