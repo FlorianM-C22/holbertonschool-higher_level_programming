4b4688f706ab203608bff053a64be3c6a43a08f8
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-22 18:15:40 +0100

    Task 6 - Matching requirements with code refactoring

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for t in range(10):
-    for u in range(t, 10):
-        if t != u:
-            print(f"{int(t)}{int(u)}", end=", " if t != 8 or u != 9 else "")
-print()  # Add a new line at the end
+for i in range(10):
+    for j in range(i, 10):
+        if i != j:
+            if i != 8 or j != 9:
+                print("{}{}, ".format(int(i), int(j)), end="")
+            else:
+                print("{}{}".format(int(i), int(j)), end="")
+print("")
