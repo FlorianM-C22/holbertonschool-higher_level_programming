206fc538f9896682b68a72c39b12638a88d45084
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-22 20:30:28 +0100

    Task 11 - Local test : OK

---
+++ python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def pow(a, b):
+    """
+    Computes a to the power of b and return the value.
+
+    Parameters:
+    - a (int): Number 1
+    - b (int): Number 2
+
+    Returns:
+    int:  The value of a ^ b
+
+    """
+
+    return (a ** b)
