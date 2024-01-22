9f0f1baf122de82343d1bbf2c442417b051e3bc5
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-22 20:26:14 +0100

    Task 10 - Local test : OK

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add(a, b):
+    """
+    Adds two integers and returns the result.
+
+    Parameters:
+    - a (int): Number 1
+    - b (int): Number 2
+
+    Returns:
+    int:  The sum of a + b
+
+    """
+
+    return (a + b)
