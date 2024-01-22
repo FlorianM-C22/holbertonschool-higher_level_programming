723bcdc772836ba3d129e87fda0e6f04a90ea198
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-22 18:07:59 +0100

    Task 6 trying to refactor to match requirements

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for t in range(10):
     for u in range(t, 10):
         if t != u:
-            print(f"{t}{u}", end=", " if t != 8 or u != 9 else "")
+            print(f"{int(t)}{int(u)}", end=", " if t != 8 or u != 9 else "")
 print()  # Add a new line at the end
