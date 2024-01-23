2a840fe17087b7582b49bb8f79538d731c9869d5
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-23 14:16:55 +0100

    WOW THIS IS SO INSTRUCTIVE

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,9 @@
 from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) == 0:
+    if len(argv) <= 1:
         print("{} arguments.".format(len(argv) - 1))
-    if len(argv) == 2:
+    elif len(argv) == 2:
         print("{} argument:".format(len(argv) - 1))
         print("{}: {}".format(1, argv[1]))
     else:
