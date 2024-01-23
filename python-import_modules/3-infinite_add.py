4993648efd5e723c74aa2c0196e194daf80cc367
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-23 15:21:16 +0100

    Pycodestyle for task 2 and 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ from sys import argv
 
 if __name__ == "__main__":
     result = 0
-    for i in range (1, len(argv)):
+    for i in range(1, len(argv)):
         i = int(argv[i])
         result += i
     print(result)
