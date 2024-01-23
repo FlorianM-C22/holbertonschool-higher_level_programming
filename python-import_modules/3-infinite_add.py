2e9e389474a67c4ef99facc1d5d82c12817cfb93
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-23 15:13:13 +0100

    Task 3 - Local test : OK. 1st push

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    result = 0
+    for i in range (1, len(argv)):
+        i = int(argv[i])
+        result += i
+    print(result)
