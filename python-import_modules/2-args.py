8c4a8cf68369a1494173e6202fd44e24832aa54c
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-23 13:57:58 +0100

    Task 2 - Removed count variable to use len()

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,15 +2,14 @@
 from sys import argv
 
 if __name__ == "__main__":
-    count = len(argv) - 1
 
     if len(argv) == 0:
-        print("{} arguments.".format(count))
-    if count == 1:
-        print("{} argument:".format(count))
-        print("{}: {}".format(count, argv[count]))
+        print("{} arguments.".format(len(argv) - 1))
+    if len(argv) == 2:
+        print("{} argument:".format(len(argv) - 1))
+        print("{}: {}".format(len(argv) - 1, argv[len(argv) - 1]))
     else:
-        print("{} arguments:".format(count))
+        print("{} arguments:".format(len(argv) - 1))
         i = 1
         while i < len(argv):
             print("{}: {}".format(i, argv[i]))
