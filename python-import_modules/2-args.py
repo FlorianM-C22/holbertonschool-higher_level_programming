493423f1f964cf25eab5d39fb8ef46a28b7fdfed
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-23 13:35:12 +0100

    Task 2 - Local test OK. 1st Push

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    count = len(argv) - 1
+
+    if count == 0:
+        print("{} arguments.".format(count))
+    if count == 1:
+        print("{} argument:".format(count))
+        print("{}: {}".format(count, argv[count]))
+    else:
+        print("{} arguments:".format(count))
+        i = 1
+        while i < len(argv):
+            print("{}: {}".format(i, argv[i]))
+            i += 1
