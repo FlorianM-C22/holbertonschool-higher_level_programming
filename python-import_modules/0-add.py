a669d0495a2055e3cd837f800b109ff17268d44d
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-23 11:53:12 +0100

    Added line 4 to prevent code for being executed when importing the module

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print(f"{str(a)} + {str(b)} = {str(result)}")
+import add_0
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = a + b
+    print(f"{str(a)} + {str(b)} = {str(result)}")
