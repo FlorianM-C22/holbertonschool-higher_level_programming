b97bec742f062b554fe2f10cf6ca18e512787638
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-23 14:14:02 +0100

    Task 2 - Playing hide and seek with the checker LOL SO FUNNY I'M LEARNING SO MUCH

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,15 +2,12 @@
 from sys import argv
 
 if __name__ == "__main__":
-
     if len(argv) == 0:
         print("{} arguments.".format(len(argv) - 1))
     if len(argv) == 2:
         print("{} argument:".format(len(argv) - 1))
-        print("{}: {}".format(len(argv) - 1, argv[len(argv) - 1]))
+        print("{}: {}".format(1, argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
-        i = 1
-        while i < len(argv):
+        for i in range (1, len(argv)):
             print("{}: {}".format(i, argv[i]))
-            i += 1
