30f49fe9f11cc8667f3bf4277b72deb13cd54ac0
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-25 10:42:28 +0100

    Task 6 - Trying to solve missing checks. Pycodestyle is ok. Added an exit condition for no arguments

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,6 +3,10 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 if __name__ == "__main__":
+    if len(argv) == 1:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
     a = int(argv[1])
     b = int(argv[3])
 
@@ -19,4 +23,4 @@ if __name__ == "__main__":
         print("{} / {} = {}".format(a, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
