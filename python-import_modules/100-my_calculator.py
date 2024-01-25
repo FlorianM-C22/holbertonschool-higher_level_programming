feab90564d8e2ccb9666a1194399ee947d3fe6d0
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-25 10:46:08 +0100

    Task 6 - Still some checks missing. Trying to refactor the code

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -3,17 +3,15 @@ from calculator_1 import add, sub, mul, div
 from sys import argv
 
 if __name__ == "__main__":
-    if len(argv) == 1:
+
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
 
-    if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    elif argv[2] == '+':
+    if argv[2] == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif argv[2] == '-':
         print("{} - {} = {}".format(a, b, sub(a, b)))
