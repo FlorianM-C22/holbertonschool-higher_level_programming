4c57315e16eb37c64430bf86c7ee75d532a95aaf
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-29 12:40:25 +0100

    And I DON'T KNOW WHAT'S WRONG LOL

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    """
-    Retrieves an element from a list.
-
-    Parameters:
-    - my_list: A list of integers
-    - idx: Index of the list
-
-    Returns:
-    An element stored in my_list at the given index idx
-    None if idx is negative or out of range
-
-    """
-
     if idx < 0:
         return None
     if idx > (len(my_list) - 1):
