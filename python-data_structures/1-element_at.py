3723297da3756b93b860cc391f12fd6e5cf518d1
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-29 12:42:41 +0100

    Task 1 - HI CHECKER

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    """
+    Retrieves an element from a list.
+
+    Parameters:
+    - my_list: A list of integers
+    - idx: Index of the list
+
+    Returns:
+    An element stored in my_list at the given index idx
+    None if idx is negative or out of range
+
+    """
+
     if idx < 0:
         return None
     if idx > (len(my_list) - 1):
         return None
 
-    return(my_list[idx])
+    return (my_list[idx])
