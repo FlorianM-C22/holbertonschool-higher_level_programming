e3adebc7c9c266968797c96f200a7f3a9e877576
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-29 12:39:31 +0100

    Task 1 - Still trying to figure out what's the checker asking for

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -13,8 +13,9 @@ def element_at(my_list, idx):
 
     """
 
-    if (idx > len(my_list) - 1):
+    if idx < 0:
         return None
-    if (idx < 0):
+    if idx > (len(my_list) - 1):
         return None
+
     return(my_list[idx])
