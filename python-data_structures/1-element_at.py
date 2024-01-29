63fbc5f74945ae1945eace5da500c290ac6c5a1d
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-29 12:29:16 +0100

    Task 1 - Local test : OK

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    Retrieves an element from a list.
+
+    Parameters:
+    - my_list: A list of integers
+    - idx: Index of the list
+
+    Returns:
+    An element stored in my_list at the given index idx
+    None if idx is negative or out of range
+
+    """
+
+    if (idx > len(my_list)):
+        return None
+    elif (idx < 0):
+        return None
+    else:
+        return(my_list[idx])
