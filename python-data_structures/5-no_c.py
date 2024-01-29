db0de755338114d16c6890c9ea200606ed8f03d3
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-29 13:58:20 +0100

    Task 5 - Local test : OK

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """
+    Removes all characters c and C from a string.
+
+    Parameters:
+    - my_string: A string of characters
+
+    Returns:
+    The new string
+
+    """
+
+    return(my_string.translate({ord('c'): None}))
