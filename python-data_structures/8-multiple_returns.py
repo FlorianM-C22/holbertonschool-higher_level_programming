47804c59938be3130b919d43a93214460847588c
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-29 16:09:58 +0100

    Task 8 - Local test : OK

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """
+    Returns a tuple with the length of a string and its first character.
+
+    Parameters:
+    - sentence: The sentence to be tested
+
+    Returns:
+    The length of a string and its first character
+        or
+    If the sentence is empty, the first character should be equal to None
+
+    """
+
+    if sentence == None:
+        sentence[0] = None
+    else:
+        return (len(sentence), sentence[0])
