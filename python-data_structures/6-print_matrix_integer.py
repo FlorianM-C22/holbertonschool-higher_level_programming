a77fb56e43b18ce5c25b3acc6e88b6db009b087d
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-29 14:25:28 +0100

    Task 6 - Will it check ?

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -11,7 +11,7 @@ def print_matrix_integer(matrix=[[]]):
 
     """
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            print(matrix[i][j], end=' ')
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end=' ')
         print()
