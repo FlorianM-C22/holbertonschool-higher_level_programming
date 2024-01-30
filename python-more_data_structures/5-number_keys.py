3142674679b705f88d1555c1004455447a73dc3e
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-30 13:16:37 +0100

    Task 5 - Local test : OK

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+
+    Parameters:
+    - a_dictionary: A dictionary
+
+    Returns:
+    - int: Number of keys
+
+    """
+
+    return len(a_dictionary)
