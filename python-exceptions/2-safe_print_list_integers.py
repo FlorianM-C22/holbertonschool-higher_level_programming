ba048d9c107309cc9850dfb61e8b04f7304ae517
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-31 15:36:34 +0100

    Task 2 - Trying to get the last requirement asked

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -13,6 +13,7 @@ def safe_print_list_integers(my_list=[], x=0):
     """
 
     count = 0
+
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
@@ -20,4 +21,5 @@ def safe_print_list_integers(my_list=[], x=0):
             continue
         count += 1
     print()
+
     return count
