7cd4c48e3d1bb3d83d958b2a0adaea8475f49b0d
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-31 10:42:16 +0100

    Task 11 - Deleted documentation since code shouldn't be more than 3 lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,16 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    """
-    Returns a list with all values multiplied by a number.
-
-    Parameters:
-    - my_list: A list of integers
-    - number: The multiplicator
-
-    Returns:
-    - A new list multiplied by number
-
-    """
-
     new_list = list(map(lambda item: item * number, my_list))
     return new_list
