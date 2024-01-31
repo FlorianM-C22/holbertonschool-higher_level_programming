a3f22f06a9e87f55029703fe1dd60ff7016a550c
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-31 15:35:23 +0100

    Task 2 - Local test : OK

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    Prints the first x elements of a list and only integers.
+
+    Parameters:
+    - my_list: can contain any type (integer, string, etc.)
+    - x: represents the number of elements to access in my_list
+
+    Returns:
+        - The real number of integers printed
+
+    """
+
+    count = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+        except(ValueError, TypeError):
+            continue
+        count += 1
+    print()
+    return count
