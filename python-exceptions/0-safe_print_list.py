d2616cbe52bec4639ad5830b825baeefc21d56ae
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-31 12:45:25 +0100

    Task 0 - Should work

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -12,19 +12,13 @@ def safe_print_list(my_list=[], x=0):
 
     """
 
-    try:
-        count = 0
+    count = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except IndexError:
+            break
+        count += 1
+    print()
 
-        for item in my_list:
-            print(item, end='')
-            count += 1
-
-            if count == x:
-                break
-
-    except TypeError:
-        pass
-
-    finally:
-        print()
-        return count
+    return count
