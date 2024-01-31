5bc683a3b81c9215d5e246a95a0b9dcb4d40741b
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-31 12:31:59 +0100

    Tasl 0 - Initial push

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """
+    Prints x elements of a list
+
+    Parameters:
+    - my_list: A list of any type
+    - x: The number of elements to be printed
+
+    Returns:
+    The real number of elements printed
+
+    """
+    try:
+        count = 0
+
+        for item in my_list:
+            print(item, end=" ")
+            count += 1
+
+            if count == x:
+                break
+
+    except TypeError:
+        print("Error: Non-printable element encountered!")
+
+    finally:
+        print()
+        return count
