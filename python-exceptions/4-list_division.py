770b0a7ea5e0496890fb2379ee1d9fdb8c0e9f9e
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-31 17:00:14 +0100

    Task 4 - Added a proper condition to finally block and refactored the code to make it simpler

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -17,21 +17,17 @@ def list_division(my_list_1, my_list_2, list_length):
 
     for i in range(list_length):
         try:
-            elem_list1 = my_list_1[i]
-            elem_list2 = my_list_2[i]
-            result = elem_list1 / elem_list2
-            new_list.append(result)
+            result = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("wrong type")
             result = 0
-            new_list.append(result)
         except ZeroDivisionError:
             print("division by 0")
             result = 0
-            new_list.append(result)
         except IndexError:
             print("out of range")
+            result = 0
         finally:
-            pass
+            new_list.append(result)
 
     return new_list
