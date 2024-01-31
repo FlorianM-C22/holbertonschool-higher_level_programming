c4ff2fb2c5c798c1c41911976da89d6d093354a8
FlorianM-C22 <7484@holbertonstudents.com>
2024-01-31 17:06:01 +0100

    Task 6 - Local test OK

---
+++ python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    """
+    Raises a name exception with a message.
+
+    Parameters:
+    - message: The message to display while raising an exception
+
+    Returns:
+    - The message written
+
+    """
+
+    raise NameError("C is fun")
