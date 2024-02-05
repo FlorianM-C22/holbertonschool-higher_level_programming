1c16356695bda6fdafed79d21ee0e25b0476faa3
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-05 18:14:49 +0100

    Task 6 - More refactoring to match checker

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -2,34 +2,49 @@
 """Contains a class definition of 'Square'"""
 
 
-class Square():
+class Square:
     """Definition of size in square"""
 
-    def get_size(self):
+    def __init__(self, size=0, position=(0, 0)):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        if type(position) is tuple and len(position) == 2 and\
+            type(position[0]) is int and type(position[1]) is int and\
+                position[0] >= 0 and position[1] >= 0:
+            self.__position = position
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    @property
+    def size(self):
         return self.__size
 
-    def set_size(self, value):
-        if type(value) is not int:
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not type(value) is int:
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif int(value) < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    size = property(get_size, set_size)
-
-    def get_position(self):
-        return self.__position
-
-    def set_position(self, value):
-        if type(value) is not int:
+    @position.setter
+    def position(self, value):
+        if type(value) is tuple and len(value) == 2 and\
+            type(value[0]) is int and type(value[1]) is int and\
+                value[0] >= 0 and value[1] >= 0:
+            self.__position = value
+        else:
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
 
-    position = property(get_position, set_position)
-
-    def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+    def area(self):
+        return self.__size * self.__size
 
     def area(self):
         return self.__size ** 2
