30f89f436168fa486c626df63991e5201822c138
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-05 17:33:46 +0100

    Task 4 - Local test OK with get and set method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,11 +5,11 @@
 class Square():
     """Definition of size in square"""
     def __init__(self, size=0):
-        self.size = size
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
     def area(self):
-        return (self.size * self.size)
+        return (self.__size * self.__size)
