f311f56c1374bf763791dde54511f70cf7824b7f
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-07 14:15:17 +0100

    Tasks 3,4,5,6 - Corrected a line to match pycodestyle

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -88,7 +88,9 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ""
         else:
-            return "\n".join(["#" * self.__width for _ in range(self.__height)])
+            return "\n".join(
+                ["#" * self.__width for _ in range(self.__height)]
+            )
 
     def __str__(self):
         return self.print()
