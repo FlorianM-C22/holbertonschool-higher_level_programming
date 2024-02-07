7c29456b56429eb18cf4b0ede8dfdd6ca9bf7b21
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-07 11:13:28 +0100

    Task 0 & 1 - Initial pushes. Needs to be checked but local testing is OK

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,80 @@
+class Rectangle:
+    """
+    A class that defines a rectangle.
+
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    """
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a rectangle.
+
+        """
+        if not type(height) == int:
+            raise TypeError("height must be an integer")
+        if int(height) < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = height
+
+        if not type(width) == int:
+            raise TypeError("width must be an integer")
+        if int(width) < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
+
+    @property
+    def height(self):
+        """
+        Getter for the height of the rectangle.
+
+        Returns:
+            The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter for the height of the rectangle.
+
+        Args:
+            value (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If the height is not an integer.
+            ValueError: If height is less than 0.
+        """
+        if not type(value) == int:
+            raise TypeError("height must be an integer")
+        if int(value) < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """
+        Getter for the width of the rectangle.
+
+        Returns:
+            The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for the width of the rectangle.
+
+        Args:
+            value (int): The width of the rectangle.
+
+        Raises:
+            TypeError: If the width is not an integer.
+            ValueError: If width is less than 0.
+        """
+        if not type(value) == int:
+            raise TypeError("width must be an integer")
+        if int(value) < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
