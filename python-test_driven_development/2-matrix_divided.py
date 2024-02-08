dc63486835539b47a5c19ecc4f992b8b0842dcfb
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-08 10:45:24 +0100

    Task 2 - More testing to match checker

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,7 +25,7 @@ def matrix_divided(matrix, div):
 
     """
 
-    if type(matrix) is not list:
+    if type(matrix) is not list or len(matrix) == 0:
         raise TypeError(
             ("matrix must be a matrix (list of lists) of integers/floats")
         )
