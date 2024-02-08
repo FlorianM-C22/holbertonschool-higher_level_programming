a4f5accf669b384dbf526f2c956bc3bfeb099f7a
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-08 18:03:06 +0100

    Task 9 - Should be fine now with proper counter

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -42,7 +42,7 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = width
 
-        Rectangle.number_of_instances += 1
+        self.__class__.number_of_instances += 1
 
     @property
     def height(self):
@@ -159,21 +159,20 @@ class Rectangle:
         Deletes the rectangle object.
 
         """
-        del self
-        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-        return Rectangle.number_of_instances
+        self.__class__.number_of_instances -= 1
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if not isinstance(rect_1, Rectangle):
+        if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
+        if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
+
+        if rect_2.area() > rect_1.area():
             return rect_2
+        else:
+            return rect_1
 
     @classmethod
     def square(cls, size=0):
