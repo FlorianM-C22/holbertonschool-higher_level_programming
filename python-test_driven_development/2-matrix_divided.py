0ae4c9187e7ef29f07b091f9f1daeba5a206b54c
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-08 11:25:13 +0100

    Task 1 - Trying to correct the code

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -8,26 +8,25 @@ Elements of matrix be divided by div, rounded to 2 decimal"""
 
 def matrix_divided(matrix, div):
     """
-    divides all elements of a matrix by div
+    divide a matrix by div
     """
-
-    mat_err = ("matrix must be a matrix (list of lists) of integers/floats")
-
-    if type(matrix) is not list or len(matrix) == 0:
-        raise TypeError(mat_err)
-    if type(div) is not int and type(div) is not float:
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    matrix_div = [[0] * len(row) for row in matrix]
 
+    new_matrix = []
     for row in matrix:
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
+        new_row = []
+        for value in row:
+            msg = "matrix must be a matrix (list of lists) of integers/floats"
+            if not isinstance(value, (int, float)):
+                raise TypeError(msg)
+            new_row.append(round(value / div, 2))
+        new_matrix.append(new_row)
 
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            matrix_div[i][j] = round((matrix[i][j] / div), 2)
-
-    return matrix_div
+    return (new_matrix)
