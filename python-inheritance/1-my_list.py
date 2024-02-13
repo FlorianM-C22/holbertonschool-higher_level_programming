b6f6fdf71bba63a364dd274e6faf3d14e293871b
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-13 11:29:26 +0100

    Task 0 and task 1- Local test OK

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for MyList class.
+"
+"
+"
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """Prints the list sorted (ascending sort).
+        """
+        print(sorted(self))
