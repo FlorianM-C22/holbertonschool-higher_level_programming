b60ecc52c98375fc8fce321e5cb3562bdb8ca2b9
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-13 11:39:03 +0100

    Task 1 - Added proper documentation, a for loop and a TypeError

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""Module for MyList class.
-"
+"""
+Module for MyList class.
 "
 "
 """
 
 
 class MyList(list):
+    """
+    Class MyList that inherits from list
+    """
     def print_sorted(self):
-        """Prints the list sorted (ascending sort).
         """
+        Prints the list sorted (ascending sort).
+        """
+        for element in self:
+            if type(element) is not int:
+                raise TypeError("type should be integer")
         print(sorted(self))
