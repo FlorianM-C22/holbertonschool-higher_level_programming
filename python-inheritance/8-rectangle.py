fa1a61d9238a8761d1bead485fdce4d9e0899d1a
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-13 12:56:51 +0100

    Task 8, 9, 10 - Replaced declared classes w/ import lines

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -6,29 +6,7 @@ Module for Rectangle class inherited from BaseGeometry.
 """
 
 
-class BaseGeometry():
-    """
-    Empty Class BaseGeometry.
-    """
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validates the value.
-
-        Args:
-            name: The name of the value.
-            value: The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
