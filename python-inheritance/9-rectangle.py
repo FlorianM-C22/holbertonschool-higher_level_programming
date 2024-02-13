7006cbdd7ee5a54a60588d35ad0679a5eaa3dbfe
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-13 12:42:50 +0100

    Task 9 - Local test OK

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Module for Rectangle class inherited from BaseGeometry.
+"
+"
+"""
+
+
+class BaseGeometry():
+    """
+    Empty Class BaseGeometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value.
+
+        Args:
+            name: The name of the value.
+            value: The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    Class Rectangle that inherits from BaseGeometry.
+    """
+    def __init__(self, width, height):
+        """
+        Initializes the Rectangle.
+
+        Args:
+            width: The width of the Rectangle.
+            height: The height of the Rectangle.
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+    def print(self):
+        print("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
