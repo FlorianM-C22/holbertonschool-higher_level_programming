15a26ac2868bb4a2ec99506475f08e53efb3e596
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-13 12:20:25 +0100

    Task 7 - Local test OK

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class.
+"
+"
+"""
+
+
+class BaseGeometry():
+    """
+    Empty Class BaseGeometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates the value.
+
+        Args:
+            name: The name of the value.
+            value: The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
