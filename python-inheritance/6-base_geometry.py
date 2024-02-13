1b21c3b98ca6a04fabb97a2508ec5bb2d2f70816
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-13 12:15:17 +0100

    Task 6 - Local test OK

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for BaseGeometry class.
+"
+"
+"""
+
+
+class BaseGeometry():
+    """
+    Empty Class BaseGeometry.
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
