eaeac04487110f196da5aea3c6c2cfb2c380d225
FlorianM-C22 <7484@holbertonstudents.com>
2024-02-14 13:00:22 +0100

    Task 5 - Local test OK

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,7 +11,5 @@ def save_to_json_file(my_obj, filename):
     """
     Writes an Object to a text file, using a JSON representation:
     """
-    json_obj = json.dumps(my_obj)
-
-    with open(filename, 'w') as outfile:
-        outfile.write(json_obj)
+    with open(filename, 'w', encoding="utf-8") as f:
+        return (json.dump(my_obj, f))
