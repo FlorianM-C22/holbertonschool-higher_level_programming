d0b7b4f163fb056468fdfbdc4a5538358500f877
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-17 15:11:53 +0100

    Task 5 - More tests

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW COLUMNS FROM hbtn_0c_0.first_table;
+SELECT column_name, data_type, is_nullable, column_key, column_default, extra
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
