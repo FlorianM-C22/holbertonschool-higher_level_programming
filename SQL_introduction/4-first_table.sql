58886ea6f6a92a4871ed75897dbc04e9bc111d77
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-17 15:05:15 +0100

    Trying to add comments to task 4 to see if checker likes it or not

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,6 @@
 -- creates a table called first_table in the current database in your MySQL server
+-- id (INT) - must be a number that acts as a unique identifier for the record
+-- name (VARCHAR) - must be a string that stores the name of the record
 CREATE TABLE IF NOT EXISTS first_table (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255)
