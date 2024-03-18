25c01e930e7a43b0900bba97fa02b211553d69b6
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-18 10:50:21 +0100

    Task 0

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,8 @@
+-- Connect to the MySQL server
+mysql -u root -p
+-- List privileges for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- List privileges for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Exit the MySQL server
+exit
