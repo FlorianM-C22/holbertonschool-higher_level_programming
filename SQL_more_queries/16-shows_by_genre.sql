cafef015460dd0e808fbfe08567e577229c76f12
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-18 13:05:41 +0100

    Task 16 testing something

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT shows.title AS show_title, genres.name AS genre_name
-FROM tv_shows AS shows
-LEFT JOIN tv_show_genres AS show_genres
-ON shows.id = show_genres.show_id
-LEFT JOIN tv_genres AS genres
-ON show_genres.genre_id = genres.id
-ORDER BY show_title ASC, genre_name ASC;
+SELECT A.title, C.name
+FROM tv_shows A
+LEFT JOIN tv_show_genres B
+ON A.id = B.show_id
+LEFT JOIN tv_genres C
+ON B.genre_id = C.id
+ORDER BY 1,2 ASC;
