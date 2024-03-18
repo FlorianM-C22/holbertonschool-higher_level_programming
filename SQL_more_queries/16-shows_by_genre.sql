0a8011dc2b8290616abedf2f1493b2ad1206dcfa
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-18 13:03:07 +0100

    Task 16

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,5 @@
 -- Lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
-SELECT tv_shows.title, COALESCE(tv_genres.name, 'NULL') AS genre
+SELECT tv_shows.title, IFNULL(tv_genres.name, 'NULL') AS genre
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT JOIN tv_genres ON tv_shows.id = tv_genres.show_id
 ORDER BY tv_shows.title ASC, genre ASC;
