abbe7c6c118f81628bb9d45938a9e3eb829c330b
FlorianM-C22 <7484@holbertonstudents.com>
2024-03-19 12:49:20 +0100

    Task 4 - Testing smth

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -5,14 +5,16 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
-    query = "SELECT cities.name FROM cities JOIN states ON \
-    cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id ASC"
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
+    cur = db.cursor()
+    cur.execute(
+        """SELECT cities.id, cities.name, states.name
+                FROM cities
+                INNER JOIN states ON cities.state_id = states.id
+                ORDER BY cities.id ASC;""")
+    rows = cur.fetchall()
 
-    cursor.execute(query, (argv[4],))
-    rows = cursor.fetchall()
-    print(", ".join([row[0] for row in rows]))
-    cursor.close()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
